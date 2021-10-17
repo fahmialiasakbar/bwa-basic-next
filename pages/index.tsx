4766a3c3c2cbac1013f5c4051989540dd110a459
Fahmi Akbar <fahmialiasakbar@gmail.com>
2021-10-17 09:14:44 +0700

    Move with <Link>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,16 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
-    <h1>Welcome Fahmi</h1>
+    <>
+      <header>
+        <ul>
+          <li><Link href="/"><a>Home</a></Link></li>
+          <li><Link href="/blog"><a>Blog</a></Link></li>
+          <li><Link href="/users"><a>User</a></Link></li>
+        </ul>
+      </header>
+      <h1>Welcome Fahmi</h1>
+    </>
   );
 }
